9a76369f949e87eb30435e0aae5907ed2a7b1738
Mukund mehra <mukundmehra2108@gmail.com>
2026-09-11 00:35:10 +0530

    yess

---
+++ q10.c
@@ -0,0 +1,25 @@
+/*Q10: Write a program to input time in seconds and convert it to hours:minutes:seconds format.*/
+
+/*
+Sample Test Cases:
+Input 1:
+3661
+Output 1:
+1:1:1
+
+Input 2:
+7322
+Output 2:
+2:2:2
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int s;
+    printf("number of seconds: ");
+    scanf("%d",&s);
+    
+    return 0;
+}
