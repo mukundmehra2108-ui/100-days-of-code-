6cbe47e7dd06cffe57bf68f7737dfe68fd53975d
Mukund mehra <mukundmehra2108@gmail.com>
2026-09-11 10:07:36 +0530

    adding the c programmer

---
+++ day9/q17.c
@@ -0,0 +1,25 @@
+Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+/*
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
+
+#include <stdio.h>
+#include <math.h>
+int main()
+
