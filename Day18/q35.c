aeb7d33e453c6d1765d1238dbf02ea6272ebc72f
Mukund mehra <mukundmehra2108@gmail.com>
2026-09-16 14:23:46 +0530

    yess

--- Day18/q35.c
+++ Day18/q35.c
@@ -1,18 +1,6 @@
 /*Q35: Write a program to print all factors of a given number.*/
 
-/*
-Sample Test Cases:
-Input 1:
-6
-Output 1:
-1 2 3 6
 
-Input 2:
-10
-Output 2:
-1 2 5 10
-
-*/
 #include <stdio.h>
 int main() { 
     int num;
